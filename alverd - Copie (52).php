f2e5bd152e7a394e43a5910b47d2fa69f6ed35a2
kchachda <43914736+kchachda@users.noreply.github.com>
2019-12-08 08:55:20 +0000

    Add files via upload

---
+++ alverd - Copie (52).php
@@ -0,0 +1,13 @@
+- **Database:** PostgreSQL
+- **API:** TWILIO
+
+## Features
+It provides features like : 
+- Scans business card image provided by user.
+Get your own secret keys 🔑 for Flickr and Twilio. Save them to a file secrets.py. Your file should look something like this
+
+```sh
+TWILIO_SID = 'abc'
+TWILIO_AUTH = 'abc'
+```
+Create database 'bizCardBook'
